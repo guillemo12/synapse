86cc32e7bf9f6cdce08c2de598a43f74e37b2352
guillemo12 <60621778+guillemo12@users.noreply.github.com>
2026-08-13 18:17:16 +0000

    ⚡ performance: remove blocking I/O in url_previewer

--- synapse/media/url_previewer.py
+++ synapse/media/url_previewer.py
@@ -28,6 +28,7 @@ import re
 import shutil
 import sys
 import traceback
+from pathlib import Path
 from typing import TYPE_CHECKING, BinaryIO, Iterable
 from urllib.parse import urljoin, urlparse, urlsplit
 from urllib.request import urlopen
@@ -39,7 +40,11 @@ from twisted.internet.error import DNSLookupError
 
 from synapse.api.errors import Codes, SynapseError
 from synapse.http.client import SimpleHttpClient
-from synapse.logging.context import make_deferred_yieldable, run_in_background
+from synapse.logging.context import (
+    defer_to_thread,
+    make_deferred_yieldable,
+    run_in_background,
+)
 from synapse.media._base import FileInfo, get_filename_from_headers
 from synapse.media.media_storage import MediaStorage, SHA256TransparentIOWriter
 from synapse.media.oembed import OEmbedProvider
@@ -296,8 +301,9 @@ class UrlPreviewer:
         elif _is_html(media_info.media_type):
             # TODO: somehow stop a big HTML tree from exploding synapse's RAM
 
-            with open(media_info.filename, "rb") as file:
-                body = file.read()
+            body = await make_deferred_yieldable(
+                defer_to_thread(self.hs.get_reactor(), Path(media_info.filename).read_bytes)
+            )
 
             tree = decode_body(body, media_info.uri, media_info.media_type)
             if tree is not None:
@@ -734,8 +740,9 @@ class UrlPreviewer:
         if not _is_json(media_info.media_type):
             return {}, None, expiration_ms
 
-        with open(media_info.filename, "rb") as file:
-            body = file.read()
+        body = await make_deferred_yieldable(
+            defer_to_thread(self.hs.get_reactor(), Path(media_info.filename).read_bytes)
+        )
 
         oembed_response = self._oembed.parse_oembed_response(url, body)
         open_graph_result = oembed_response.open_graph_result
